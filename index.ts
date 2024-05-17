402533a743db087a1345c422cf2acb528e43f620
T_DaMER <dpancov64@gmail.com>
2024-05-17 15:36:31 +0300

    Tips and allowance

--- index.ts
+++ index.ts
@@ -1,34 +1,41 @@
 import neynarClient from './utils/neynarClient'
 
-const file = Bun.file('./allowance_data.json')
+const tipsFile = Bun.file('./tips_data.json')
+const allowanceFile = Bun.file('./allowance_data.json')
 
 const server = Bun.serve({
   port: 3000,
   async fetch(req) {
     try {
-      const allowances = await file.json()
-      const body = await req.text()
-      const hookData = JSON.parse(body)
-
       if (!process.env.SIGNER_UUID) {
         throw new Error('Make sure you set SIGNER_UUID in your .env file')
       }
 
+      const body = await req.text()
+      const hookData = JSON.parse(body)
       const username = hookData?.data?.author?.username
 
       if (!username) {
         throw new Error("Wrong request, username wasn't provided")
       }
 
-      const tips = allowances[username]
+      const tipsParsed = await tipsFile.json()
+      const tips = tipsParsed[username]
 
-      if (!tips) {
-        throw new Error('User allowance not found or tips are zero')
+      if (tips === undefined) {
+        throw new Error('User tips not found')
+      }
+
+      const allowanceParsed = await allowanceFile.json()
+      const allowance = allowanceParsed[username]
+
+      if (allowance === undefined) {
+        throw new Error('Allowance not found')
       }
 
       const reply = await neynarClient.publishCast(
         process.env.SIGNER_UUID,
-        `${username} Tips received: ${tips}`,
+        `${username} Tips received: ${tips} NEGED \n Allowance: ${allowance} NEGED`,
         {
           embeds: [],
           replyTo: hookData.data.hash,
